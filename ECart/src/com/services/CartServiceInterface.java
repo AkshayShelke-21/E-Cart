9d5cb4b97f2f5bef3ea3c5779e3fcd2a8f1b3946
sarthak <sarthakgosawar25@gmail.com>
2023-01-14 12:42:56 +0530

    Cart and Order Service Interface Updated

--- ECart/src/com/services/CartServiceInterface.java
+++ ECart/src/com/services/CartServiceInterface.java
@@ -1,5 +1,16 @@
 package com.services;
 
-public class CartServiceInterface {
+import java.util.List;
 
+import com.entities.Cart;
+import com.struct.CartStruct;
+
+
+public interface CartServiceInterface {
+
+	int increaseOtyAndPriceInCartWhenProductIsAddedInCart(String token, Cart cart, int cartId);
+
+	int addToCart(String token, Cart cart);
+
+	List<CartStruct> getCartDetails(String token);
 }
--- ECart/src/com/services/OrderServiceInterface.java
+++ ECart/src/com/services/OrderServiceInterface.java
@@ -1,5 +1,14 @@
 package com.services;
 
-public class OrderServiceInterface {
+import java.util.List;
 
+import com.entities.Order;
+import com.struct.OrderStruct;
+
+
+public interface OrderServiceInterface {
+
+	int placeOrder(String token, Order order);
+
+	List<OrderStruct> checkuserOrderHistory(int userId, String token);
 }
