a77fcc63f8056a7b73ee2bfe94619ada143ae3c5
AkshayShelke21 <akshelke21@gmail.com>
2023-01-14 06:44:03 +0530

    Entities User and UserLogin added.

--- ECart/src/com/entities/User.java
+++ ECart/src/com/entities/User.java
@@ -1,5 +1,38 @@
 package com.entities;
 
 public class User {
-
+	
+	//Elements of User Table
+	private int userId;
+	private String username;
+	private boolean isAdmin = false;
+	private String password;
+	
+	//Getter and Setter Methods
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
--- ECart/src/com/entities/UserLogin.java
+++ ECart/src/com/entities/UserLogin.java
@@ -1,5 +1,37 @@
 package com.entities;
 
 public class UserLogin {
-
+	//Entities for UserLogin class
+	private String username;
+	private String password;
+	
+	//Constructor with User object argument
+	public UserLogin(User user){
+		this.username = user.getUsername();
+		this.password = user.getPassword();
+	}
+	
+	//No argument constructor
+	public UserLogin() {}
+	
+	//Constructor with username and password argument
+	public UserLogin(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	
+	//Getter and Setter Methods
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
